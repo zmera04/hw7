5b27adb76419c13248ef3fe9194c8411ee4d5cac
Nathan Hishon <nathan.hishon@gmail.com>
2024-02-29 09:40:16 -0500

    todo tweaks

--- app/controllers/PostController.php
+++ app/controllers/PostController.php
@@ -4,9 +4,9 @@ namespace app\controllers;
 use app\core\Controller;
 class PostController extends Controller
 {
-//todo make a method to return some posts
+//todo make a method to return some posts, post objects should come from the post model class
 //also need to make a twig template to show the posts
 //look at the users controller/template for an example
-//of how to parse a php array in twig
+
 
 }
--- public/index.php
+++ public/index.php
@@ -13,6 +13,6 @@ use app\controllers\PostController;
 $url = $_SERVER["REQUEST_URI"];
 
 //todo add a switch statement router to route based on the url
-//if it is /posts return an array of posts
-//if it is / return the homepage view via the main controller
-//if it is something else return 404
+//if it is "/posts" return an array of posts via the post controller
+//if it is "/" return the homepage view from the main controller
+//if it is something else return a 404 view from the main controller
